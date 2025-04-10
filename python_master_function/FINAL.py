49f9239c556f2f956292e061a213bb3159b0219b
Jonas Mago <jonas.h.mago@gmail.com>
2025-04-10 19:35:48 -0400

    edit path

--- python_master_function/FINAL.py
+++ python_master_function/FINAL.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
         results_table_path = f'output/results_c_{name}/summary.csv'
         results_dict_dir = f'output/results_c_{name}/details/'
         os.makedirs(results_dict_dir, exist_ok=True)
-        paths = glob.glob('/home/jmago/projects/def-michael9/jmago/jhana_eeg/data_test/03s/*.fif')
+        paths = glob.glob('/home/jmago/projects/def-michael9/jmago/jhana_eeg/data/10s/*.fif')
         paths.sort()
         print (paths)
 
