fce7cca00aad02ac6f5a7f2c07ddc649f15e9f95
Jonas Mago <jonas.h.mago@gmail.com>
2025-04-10 11:00:40 -0400

    hurst print

--- python_master_function/features_DFA.py
+++ python_master_function/features_DFA.py
@@ -29,7 +29,7 @@ def get_channel_hurst(ch_data,sfreq):
     
     hurst_fh, _ = nk.fractal_hurst(amplitude_envelope, scale=scale, show=False)
     hurst_dfa, _ = nk.fractal_dfa(amplitude_envelope, scale=scale, show=False)
-    
+    print ('done one round of hurst')
     return  hurst_fh, hurst_dfa
 
 
